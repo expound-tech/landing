7a7dad499ef5210e76295e500f40a9087ac9e476
lonexw <lone@yousails.com>
2022-10-16 22:02:15 +0800

    add website ICP info

--- src/_includes/footer.11ty.cjs
+++ src/_includes/footer.11ty.cjs
@@ -1,9 +1,11 @@
 module.exports = function (data) {
   return `
 <footer>
-  <p>
+  <p><small>
     Created By 
-    <a target="_blank" href="https://phab.xyz">Phab.xyz</a>, Since 2022
+    <a target="_blank" href="https://phab.xyz">Phab.xyz</a>, Since 2022 |
+    蜀 ICP 备 2022024415 号
+    </small>
   </p>
 </footer>`;
 };
