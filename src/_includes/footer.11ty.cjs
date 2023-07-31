d5d8fdb7b4b52c59961b3ed7668febfa3b7e17b9
lonexw <lone@yousails.com>
2023-07-31 20:12:25 +0800

    处理备案信息核查问题

--- src/_includes/footer.11ty.cjs
+++ src/_includes/footer.11ty.cjs
@@ -4,7 +4,7 @@ module.exports = function (data) {
   <p><small>
     Created By 
     <a target="_blank" href="https://phab.xyz">Phab.xyz</a>, Since 2022 |
-    蜀 ICP 备 2022024415 号
+    <a target="_blank" href="https://beian.miit.gov.cn/">蜀 ICP 备 2022024415 号</a>
     </small>
   </p>
 </footer>`;
